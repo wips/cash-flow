7442a1bf0e50c3719ed3537f418f3126dbcf4a99
Viktor Molokostov <wips84@gmail.com>
2018-01-13 17:50:19 +0200

    card-6633705 - tag config should accept lists

--- src/tagging/get-tags.ts
+++ src/tagging/get-tags.ts
@@ -1,17 +1,24 @@
 import getTaggerRegistry from './get-tagger-registry';
 
 export default function getTags(transaction: TTransaction): string[] {
-  const { details } = transaction;
   const tags: string[] = [];
   for (const key of getTaggerRegistry().keys()) {
-    const matcher = getTaggerRegistry().get(key);
-    if (typeof matcher === 'string' && details === matcher) {
-      tags.push(key);
-    } else if (matcher instanceof RegExp && typeof matcher.test === 'function' && matcher.test(details)) {
-      tags.push(key);
-    } else if (typeof matcher === 'function' && matcher(transaction)) {
-      tags.push(key);
-    }
+    tags.push(...getTagsByMatcher(key, getTaggerRegistry().get(key), transaction));
+  }
+  return tags;
+}
+
+function getTagsByMatcher(tag: string, matcher: TTagMatcher, transaction: TTransaction): string[] {
+  const { details } = transaction;
+  const tags: string[] = [];
+  if (typeof matcher === 'string' && details === matcher) {
+    tags.push(tag);
+  } else if (matcher instanceof RegExp && typeof matcher.test === 'function' && matcher.test(details)) {
+    tags.push(tag);
+  } else if (typeof matcher === 'function' && matcher(transaction)) {
+    tags.push(tag);
+  } else if (Array.isArray(matcher)) {
+    matcher.forEach(matchingItem => tags.push(...getTagsByMatcher(tag, matchingItem, transaction)));
   }
   return tags;
 }
